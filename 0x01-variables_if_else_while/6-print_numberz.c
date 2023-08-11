3d49557aae831921ea9f80a547b109c7d0bfc17d
Vuyisanani <vuyisanani@ymail.com>
2023-08-11 09:12:35 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
-/**
- * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line, using putchar
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-
-	for (n = 0; n <= 9; n++)
-	
-	putchar(n);
-	putchar('\n');
-	return (0);
-}
