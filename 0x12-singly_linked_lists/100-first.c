c0a9e00f0b6fa0600413680df2ec4e035eff353b
Vuyisanani <vuyisanani@ymail.com>
2023-09-21 19:05:53 -0700

    5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * print_text - the function to print the desired text.
+ * execute function
+ */
+
+void print_text(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
