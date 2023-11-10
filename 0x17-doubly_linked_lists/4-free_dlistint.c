25327ff8cf5d37bd34f3769f072a529987298378
Vuyisanani <vuyisanani@ymail.com>
2023-11-10 13:15:04 -0800

    Task 4

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/*
+ * free_dlistint - will free linked dlistint_t list.
+ * @head: head of dlistint_t list.
+ */
+
+void free_dlistint(dlistint_t *head)
+
+{
+	dlistint_t *tmp;
+
+	while (head)
+
+	{
+	tmp = head->next;
+	free(head);
+	head = tmp;
+	}
+}
